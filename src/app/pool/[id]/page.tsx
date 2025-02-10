c0207dba04de8ea4bbcfc28251fd062c5beec548
ninjazan420 <dermonjo@gmail.com>
2025-02-10 18:45:53 +0100

    fixed pools const id = await Promise.resolve(params.id) error

--- src/app/pool/[id]/page.tsx
+++ src/app/pool/[id]/page.tsx
@@ -6,16 +6,22 @@ interface Props {
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  // Warte auf die params
+  const id = (await params).id;
+
   return {
-    title: `Pool #${params.id} - f0ck beta v1`,
-    description: `View collection #${params.id} and its contents.`,
+    title: `Pool #${id} - f0ck beta v1`,
+    description: `View collection #${id} and its contents.`,
   };
 }
 
 export default async function Pool({ params }: Props) {
+  // Warte auf die params
+  const id = (await params).id;
+
   return (
     <div className="container mx-auto px-4 py-6">
-      <PoolDetails poolId={params.id} />
+      <PoolDetails poolId={id} />
     </div>
   );
 }
